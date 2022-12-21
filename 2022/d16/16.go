4b2d9d6d22297a0737015ae7aefcc2fe0f35330b
Michal Pirchala <michal.pirchala@cloudtalk.io>
2022-12-21 20:33:10 +0100

    day16

--- 2022/d16/16.go
+++ 2022/d16/16.go
@@ -3,7 +3,7 @@ package d16
 import (
 	"aoc2022/common"
 	"fmt"
-	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -12,83 +12,102 @@ type valve struct {
 	leadsTo []string
 }
 
-type sim struct {
-	time      int
-	opened    map[string]bool
-	valve     string
-	pressure  int
-	pressureT int
+type distance struct {
+	distance int
+	valve    string
 }
 
+var (
+	valves   = make(map[string]valve, 0)
+	dists    = make(map[string]map[string]int)
+	nonempty = make([]string, 0)
+	indices  = make(map[string]int)
+	cache    = make(map[string]int)
+)
+
+// P1 basically rewrites hyper-neutrino's excellent solution
+// source: https://www.youtube.com/watch?v=bLMj50cpOug&t=131s
 func P1() {
-	valves := make(map[string]valve, 0)
-	q := make([]sim, 0)
-	v := 0
 	common.FileIter("16", func(s string) {
 		name, valve := scan(s)
 		valves[name] = valve
-		if v == 0 {
-			q = append(q, sim{
-				time:   30,
-				opened: make(map[string]bool),
-				valve:  name,
-			})
-		}
-		v++
 	})
 
-	ps := make([]int, 0)
-	for len(q) > 0 {
-		a := q[0]
-		q = q[1:]
-
-		//check for time
-		if a.time <= 0 {
-			ps = append(ps, a.pressureT)
+	for valve, _ := range valves {
+		if valve != "AA" && valves[valve].rate == 0 {
 			continue
 		}
+		if valve != "AA" {
+			nonempty = append(nonempty, valve)
+		}
+		dists[valve] = map[string]int{
+			"AA":  0,
+			valve: 0,
+		}
+		visited := make(map[string]bool)
 
-		// skip if actual is opened
-		if a.opened[a.valve] || valves[a.valve].rate == 0 {
-			//move to others without opening the valve
-			for _, next := range valves[a.valve].leadsTo {
-				q = append(q, sim{
-					time:      a.time - 1,
-					opened:    copyOpened(a.opened),
-					valve:     next,
-					pressure:  a.pressure,
-					pressureT: a.pressureT + a.pressure,
-				})
+		q := make([]distance, 0)
+		q = append(q, distance{distance: 0, valve: valve})
+
+		for len(q) > 0 {
+			a := q[0]
+			q = q[1:]
+			for _, neighbor := range valves[a.valve].leadsTo {
+				if visited[neighbor] {
+					continue
+				}
+				visited[neighbor] = true
+				if valves[neighbor].rate > 0 {
+					dists[valve][neighbor] = a.distance + 1
+				}
+				q = append(q, distance{distance: a.distance + 1, valve: neighbor})
 			}
-			continue
 		}
 
-		// open actual valve
-		a.time -= 1
-		a.pressureT += a.pressure
-		a.opened[a.valve] = true
-		a.pressure += valves[a.valve].rate
-
-		//fmt.Println(a.pressure)
-
-		if a.time == 0 {
-			q = append(q, a)
-			continue
+		if valve != "AA" {
+			delete(dists[valve], "AA")
 		}
-
-		for _, next := range valves[a.valve].leadsTo {
-			q = append(q, sim{
-				time:      a.time - 1,
-				opened:    copyOpened(a.opened, a.valve),
-				valve:     next,
-				pressure:  a.pressure,
-				pressureT: a.pressureT + a.pressure,
-			})
-		}
-		//fmt.Println(len(q))
+		delete(dists[valve], valve)
 	}
-	sort.Ints(ps)
-	fmt.Println(ps[len(ps)-1])
+
+	fmt.Println(dists)
+
+	for i, v := range nonempty {
+		indices[v] = i
+	}
+
+	fmt.Println(dfs(30, "AA", 0))
+
+	b := (1 << len(nonempty)) - 1
+	m := 0
+	for i := 0; i < (b+1)/2; i++ {
+		a := dfs(26, "AA", i) + dfs(26, "AA", b^i)
+		if a > m {
+			m = a
+		}
+	}
+	fmt.Println(m)
+}
+
+func dfs(time int, valve string, bitmask int) int {
+	cacheKey := strconv.Itoa(time) + valve + strconv.Itoa(bitmask)
+	if val, ok := cache[cacheKey]; ok {
+		return val
+	}
+	maxval := 0
+	for neighbor := range dists[valve] {
+		bit := 1 << indices[neighbor]
+		if bitmask&bit > 0 {
+			continue
+		}
+		remtime := time - dists[valve][neighbor] - 1
+		if remtime <= 0 {
+			continue
+		}
+		maxval = common.Max(maxval, dfs(remtime, neighbor, bitmask|bit)+valves[neighbor].rate*remtime)
+	}
+	cache[cacheKey] = maxval
+	return maxval
 }
 
 func scan(l string) (string, valve) {
@@ -103,19 +122,4 @@ func scan(l string) (string, valve) {
 		rate:    common.StrToInt(val[1]),
 		leadsTo: valves,
 	}
-}
-
-func copyOpened(o map[string]bool, valves ...string) map[string]bool {
-	openedCopy := make(map[string]bool)
-	for s, _ := range o {
-		openedCopy[s] = true
-	}
-	for _, s := range valves {
-		openedCopy[s] = true
-	}
-	return openedCopy
-}
-
-func P2() {
-
 }
